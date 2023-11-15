dfb2379304cda08dfb43356c6c1a86ca8fc3116d
Mike <boxingbat@gmail.com>
2023-11-15 20:41:14 +0800

    color text

--- CaveTickers/CaveTickers/NewsViewController.swift
+++ CaveTickers/CaveTickers/NewsViewController.swift
@@ -11,7 +11,7 @@ class NewsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = .systemPink
         // Do any additional setup after loading the view.
     }
     
--- CaveTickers/CaveTickers/SearchViewController.swift
+++ CaveTickers/CaveTickers/SearchViewController.swift
@@ -11,6 +11,7 @@ class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .lightGray
 
         // Do any additional setup after loading the view.
     }
