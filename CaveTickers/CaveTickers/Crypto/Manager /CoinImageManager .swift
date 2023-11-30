86fd698eceda6d889867aa1042cc892f3f084e00
Mike <boxingbat@gmail.com>
2023-11-30 13:21:58 +0800

    setUp cache for coin images

--- CaveTickers/CaveTickers/Crypto/Manager /CoinImageManager .swift
+++ CaveTickers/CaveTickers/Crypto/Manager /CoinImageManager .swift
@@ -13,20 +13,36 @@ class CoinImageManager {
     @Published var image: UIImage?
     private var imageSubscription: AnyCancellable?
     private let coin: CoinModel
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "coin_images"
+    private let imageName: String
 
     init(coin: CoinModel) {
         self.coin = coin
+        self.imageName = coin.id
         getCoinImage()
     }
-    private func getCoinImage () {
+    private func getCoinImage() {
+        if let savedImage = fileManager.getImage(imageName: coin.id, folderName: folderName) {
+            image = savedImage
+            print("Retrieved image for File Manager!")
+        } else {
+            downloadCoinImage()
+            print("Downloading image now")
+        }
+    }
+
+    private func downloadCoinImage () {
         guard let url = URL(string: coin.image) else { return }
         imageSubscription = NetworkingManager.download(url: url)
             .tryMap({ (data) -> UIImage? in
                 return UIImage(data: data)
             })
-            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self](returnedImage) in
-                self?.image = returnedImage
-                self?.imageSubscription?.cancel()
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in
+                guard let self = self, let downloadedImage = returnedImage else { return }
+                self.image = downloadedImage
+                self.imageSubscription?.cancel()
+                self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
             })
     }
 }
